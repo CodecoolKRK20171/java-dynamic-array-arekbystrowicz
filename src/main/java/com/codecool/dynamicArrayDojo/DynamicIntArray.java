f0505fde37b27823697f56e0aa15e75cbe84aa02
abystry <arek.bystrowicz@gmail.com>
2018-01-22 18:56:51 +0100

    Add fields and constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,24 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
+
 public class DynamicIntArray {
 
+    private static final int DEFAULT = 1;
+
+    private int[] array;
+    private int size;
+    private int index;
+
+    public DynamicIntArray() {
+        this.array = new int[DEFAULT];
+        this.size = DEFAULT;
+        this.index = 0;
+    }
+
+    public DynamicIntArray(int size) {
+        this.array = new int[size];
+        this.size = size;
+        this.index = 0;
+    }
+    
 }
